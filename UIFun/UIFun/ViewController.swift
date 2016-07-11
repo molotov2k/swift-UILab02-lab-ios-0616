d8a5b70649c375fd6b4c4b70069e4c5e613af5d2
molotov2k <molotov2k@gmail.com>
2016-07-11 17:17:20 -0400

    done

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -9,19 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // TODO: Set up IB outlets
 
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var topSegment: UISegmentedControl!
+    @IBOutlet weak var botSegment: UISegmentedControl!
+    
+    var topSelection = "Red"
+    var botSelection = "Red"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: Set the initial paint color to "red"
+        topSegment.accessibilityIdentifier = "Top"
+        botSegment.accessibilityIdentifier = "Bot"
     }
 
-    func mixColors(withFirst first: String, second: String) -> String {
-        // TODO: Mix colors and return a string indicating the name of the mixed color
-        // (e.g., "red", "purple", "blue")
+    func mixColors() {
+        var color = paintBucket.backgroundColor
+        
+        switch (topSelection, botSelection) {
+            
+        case ("Red", "Red"): color = UIColor.redColor()
+        case ("Red","Yellow"): color = UIColor.orangeColor()
+        case ("Red","Blue"): color = UIColor.purpleColor()
+        
+        case ("Yellow", "Red"): color = UIColor.orangeColor()
+        case ("Yellow", "Yellow"): color = UIColor.yellowColor()
+        case ("Yellow","Blue"): color = UIColor.greenColor()
+            
+        
+        case ("Blue","Red"): color = UIColor.purpleColor()
+        case ("Blue","Yellow"): color = UIColor.greenColor()
+        case ("Blue","Blue"): color = UIColor.blueColor()
+        
+        default: color = UIColor.clearColor()
+            
+        }
+        
+        paintBucket.backgroundColor = color
+        
     }
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
-        // TODO: Mix each selected color and set the paint color to the mixed color
+        let segment = sender.selectedSegmentIndex
+        if sender.accessibilityIdentifier == "Top" {
+            topSelection = sender.titleForSegmentAtIndex(segment)!
+        } else {
+            botSelection = sender.titleForSegmentAtIndex(segment)!
+        }
+        mixColors()
     }
+    
 }
